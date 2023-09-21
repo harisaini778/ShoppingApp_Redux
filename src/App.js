602ad0a60eaec8afacbfe098781cf65519eb35f6
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-09-21 15:41:55 +0530

    used fetch toput the cart data in the cart of firebase api

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import NavBar from './components/Navbar';
 import CartItems from "./components/CartItems";
 import Products from './components/Products';
 import { useSelector } from "react-redux";
+import { useEffect } from 'react';
 
 function App() {
 
- const showCart =  useSelector((state) => state.ui.cartIsVisible);
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart.items);
+
+  useEffect(() => {
+    fetch("https://redux-shopping-app-88c0e-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body:JSON.stringify(cart),
+    })
+  },[cart]) 
 
   return (
     <div className="App">
