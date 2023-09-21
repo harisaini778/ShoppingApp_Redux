908a31038866e7d8f71e98bbf6aa52632042c204
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-09-21 16:41:36 +0530

    added a new reducer function shownotification in the uislice components, and added a logic to show notification status based on the response recieved from the firebase api

--- src/App.js
+++ src/App.js
@@ -2,20 +2,56 @@ import './App.css';
 import NavBar from './components/Navbar';
 import CartItems from "./components/CartItems";
 import Products from './components/Products';
-import { useSelector } from "react-redux";
+import { useSelector,useDispatch } from "react-redux";
 import { useEffect } from 'react';
+import { UiActions } from './store/ui-slice';
 
 function App() {
 
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const notification = useSelector((state) => state.ui.showNotification);
+  
   const cart = useSelector((state) => state.cart.items);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    fetch("https://redux-shopping-app-88c0e-default-rtdb.firebaseio.com/cart.json", {
-      method: "PUT",
-      body:JSON.stringify(cart),
-    })
-  },[cart]) 
+    const sendCartData = async () => {
+
+      dispatch(UiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        messagec: "Sending cart data!"
+      }));
+      
+      const response = await fetch("https://redux-shopping-app-88c0e-default-rtdb.firebaseio.com/cart.json", {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      });
+      
+      if (!response.ok) {
+        throw new Error("Sending cart data failed")
+      } else {
+        dispatch(UiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully",
+        }));
+      }
+      sendCartData().catch((error) => {
+        dispatch(
+          UiActions.showNotification({
+            status: "failed",
+            title: "Failed!",
+            message:"Sendind cart data failed!"
+         })
+       )
+     })
+      
+    };
+  },[cart,dispatch]) 
+
+};
+   
 
   return (
     <div className="App">
@@ -24,6 +60,6 @@ function App() {
       <Products/>
     </div>
   );
-}
+
 
 export default App;
